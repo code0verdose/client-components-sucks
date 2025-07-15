08247691e33a1fa4a062caf7418e963948fc724a
codeoverdose <mydogfromhell666@gmail.com>
2025-07-15 18:55:18 +0300

    feat: add page and section layouts, update layout imports, and refactor index pages to use new layouts

---
+++ src/layouts/index.ts
@@ -0,0 +1,4 @@
+export { default as BaseHead } from './base-head.layout.astro'
+export { default as BlogPostLayout } from './blog-post.layout.astro'
+export { default as PageLayout } from './page.layout.astro'
+export { default as SectionLayout } from './section.layout.astro'
